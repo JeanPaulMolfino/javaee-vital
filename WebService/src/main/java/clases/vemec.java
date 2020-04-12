cc6dabe1ebf5ae7f2513d0e12fb4dc0e4e958557
Jean Paul Molfino <jeanpaulmolfino@gmail.com>
2020-04-11 23:19:54 -0300

    CRUD-L de VEmecs listo
    Aun sin poder testear debidamente, caresco de la base de datos y demas aun

--- WebService/src/main/java/clases/vemec.java
+++ WebService/src/main/java/clases/vemec.java
@@ -10,5 +10,58 @@ package clases;
  * @author javaee
  */
 public class vemec {
+    private int id;
+    private String marca;
+    private String modelo;
+    private String ubicacion;
+
+    
+
+    public vemec() {
+    }
+
+    public vemec(int id, String marca, String modelo, String ubicacion) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ubicacion = ubicacion;
+    }
+
+    @Override
+    public String toString() {
+        return "vemec{" + "id=" + getId() + ", marca=" + getMarca() + ", modelo=" + getModelo() + ", ubicacion=" + getUbicacion() + '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
     
 }
--- WebService/src/main/java/controller/controller_vemec.java
+++ WebService/src/main/java/controller/controller_vemec.java
@@ -1,5 +1,87 @@
 package controller;
 
+import Config.conexion;
+import clases.vemec;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class controller_vemec {
+    conexion con = new conexion();
+    JdbcTemplate jdbcTemplate = new JdbcTemplate(con.Conectar());
+    
+    public Boolean create_vemec(String marca, String modelo, String ubicacion){
+        String[] variables = {marca,modelo,ubicacion};
+        try{
+            String sql = "insert into vemec(marca, modelo, ubicacion) values (?,?,?)";
+            this.jdbcTemplate.update(sql, variables);
+            return true;
+        }catch(Exception e){
+            
+        }
+        return false;
+    }
+    
+    public Boolean update_vemec(vemec selected){
+        String[] variables = {selected.getMarca(), selected.getModelo(), selected.getUbicacion(), Integer.toString(selected.getId())};
+        try{
+            String sql = "update vemecs set marca=?, modelo=?, ubicacion=? where id = ?";
+            this.jdbcTemplate.update(sql, variables);
+            return true;
+        }catch(Exception e){
+            
+        }
+        return false;
+    }
+    
+    public Boolean delete_vemec(vemec selected){
+        String[] variables = {Integer.toString(selected.getId())};
+        try{
+            String sql = "delete from mensajes where id = ?";
+            this.jdbcTemplate.update(sql, variables);
+            return true;
+        }catch(Exception e){
+            
+        }
+        return false;
+    }
+    
+    public List<vemec> read_vemec(){
+        String sql = "select * from vemecs";
+        List<vemec> datos = null;
+        try{
+        datos = this.jdbcTemplate.query(sql, new BeanPropertyRowMapper(vemec.class));
+        }catch(Exception e){
+            
+        }
+        return datos;
+    }
+    
+    public vemec find_vemec(int id){
+        String[] variables = {Integer.toString(id)};
+        vemec result = null;
+        try{
+            String sql = "delete from mensajes where id = " + id;
+            result = (vemec)this.jdbcTemplate.queryForObject(sql, 
+				new Object[] { id }, new RowMapper() {
+					public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+						vemec re = new vemec();
+						re.setId(rs.getInt("id"));
+                                                re.setMarca(rs.getString("marca"));
+                                                re.setModelo(rs.getString("modelo"));
+                                                re.setUbicacion(rs.getString("ubicacion"));
+						return re;
+					}
+				});
+        }catch(Exception e){
+            
+        }
+        return result;
+    }
     
 }
